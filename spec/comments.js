c9fa8dae54fc606ec1e5114a41c7a23cc40c2e78
Samy Pesse <samypesse@gmail.com>
2016-08-31 20:17:38 +0200

    Add more tests in spec

---
+++ spec/comments.js
@@ -0,0 +1,14 @@
+
+// Comments should start with a space.
+
+/* Always ! */
+
+/**
+ * JSDoc comment should start with 2 *
+ * And comment do not require lines before functions
+ */
+function myFunction() {
+
+}
+
+module.exports = myFunction;
---
+++ spec/functions.js
@@ -0,0 +1,15 @@
+
+// Named function
+function sayHello(msg) {
+    return 'Hello ' + msg;
+}
+
+// Anonymous function
+const direBonjour = function(msg) {
+    return 'Bonjour ' + msg;
+};
+
+module.exports = {
+    sayHello,
+    direBonjour
+};
